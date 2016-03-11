813878c03931bdb4b190300c304fe89e1ade4ee2
WangYiyao <viana37@sina.com>
2016-03-11 11:35:12 +0800

    后台获取秒数
    修改

--- timer/m_camerabox.js
+++ timer/m_camerabox.js
@@ -9,6 +9,14 @@ function getQueryString(name) {
 	return null;
 };
 var flag;
+var palyVideo;
+var tt;
+var t =0;  //setTimeout的返回标志
+var h ;
+var m ;
+var s ;
+ 
+
 function getUserLiveVideo(user, cbyes, cbno){
 	var url = "ajx/cameraBoxVideo.do";
 		$.ajax({
@@ -16,12 +24,11 @@ function getUserLiveVideo(user, cbyes, cbno){
 			url: url,
 			data: "user=" + user,
 			dataType: "JSON",
-			success: function(res){
+			success: function(res){	
+			palyVideo = res.reason;
 				
 				if(res.reason){
-					//var shareURL = "www.vtc365.cn/vid=" +res.reason;
-					//alert(shareURL);
-					$(".share-link").val("www.vtc365.cn/vid=" +res.reason);
+					$(".share-link").val("voip.vtc365.com:8080/LiveVideoServer/play.do?video.videoId=" +res.reason);
 					if(cbyes){
 						cbyes(res.reason);
 					}
@@ -34,8 +41,6 @@ function getUserLiveVideo(user, cbyes, cbno){
 			}		
 		});
 }
-
-
 function hideVideo(box, user, psk){
 	if(box){
 		box.empty();
@@ -46,30 +51,90 @@ function hideVideo(box, user, psk){
 		endtime();
 		$('.current-time').css({visibility:"hidden"});
 		startLinkdiv.append(startbox);
-		
+		var title = $('#liveName').val();
+		var groupId = $("#groupId").val();
+		var rate = $("#rate").val();
+		var description = $("#description").val();
 		startLinkdiv.click(function(){
-			
-			//修改标题
-			var name =$('#liveName').val() ;
-			var devid =$("#devid").html() ;
-			var videoId = null;
-			var data = "db.name='"+name+"'&db.devid="+devid+"&db.videoId="+videoId;
-			
-			$.ajax({
-				type: "POST",
-				url: "ajx/changeTitle.do",
-				data: data,
-				dataType: "JSON",
-				success: function(res){
-				}
-			});
-			var title = $('#liveName').val();
-			var groupId = $("#groupId").val();
-			var rate = $("#rate").val();
-			var description = $("#description").val();
+			if(status==0){
+				$('#pop').show();
+				$('#reminder').slideDown();
+				
+			}else{
+				startVideo(box,user,psk,title,groupId,rate,description);
+			}
+		});
+		box.append(startLinkdiv);
+		$('#free_use').one("click",function(){
+			console.log("startVideo!");
+			$('#pop,#reminder').hide();
 			startbox.html("<span class='starting'>启动中</span><i class='loading'></i>");
-			var url = "ajx/cameraBoxStart.do";
-			var data = "user=" + user + "&key=" + psk + "&title=" + title + "&groupId="+ groupId + "&rate="+ rate + "&desc="+ description;
+			startVideo(box,user,psk,title,groupId,rate,description)
+		});
+		
+	}
+}
+
+function startVideo(box,user,psk,title,groupId,rate,description){
+	//修改标题
+	var name =$('#liveName').val() ;
+	var devid =$("#devid").html() ;
+	var videoId = null;
+	var data = "db.name='"+name+"'&db.devid="+devid+"&db.videoId="+videoId;
+	
+	$.ajax({
+		type: "POST",
+		url: "ajx/changeTitle.do",
+		data: data,
+		dataType: "JSON",
+		success: function(res){
+		}
+	});
+	var url = "ajx/cameraBoxStart.do";
+	var data1 = "user=" + user + "&key=" + psk + "&title=" + title + "&groupId="+ groupId + "&rate="+ rate + "&desc="+ description;
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: data1,
+		dataType: "JSON",
+		success: function(res){
+			if(res){
+				setTimeout(function(){
+					getBoxResponse(user);
+					getUserLiveVideo(user, function(vid){
+						showVideo(box, vid, user, psk);
+					}, function(){
+						hideVideo(box, user, psk);
+					});
+				}, 5000);
+			}
+		}
+	});
+}
+
+function showVideo(box, vid, user, psk){
+	if(box){
+		box.empty();
+		
+		var vlinkdiv = cje("div");
+		$('#view_video').html('');
+		//vlinkdiv.append(cje("a").attr("href", "play.do?video.videoId=" + vid).attr("target", "_blank").text("观看"));
+		$('#view_video').append(cje("a").attr("href", "play.do?video.videoId=" + vid).attr("target", "_blank").text("观看"));
+		//$('.view-area').attr("href", "play.do?video.videoId=" + vid).attr("target", "_blank"));
+		var stopLinkdiv = cje("div");
+		var stopbox = cje("div").html("<span class='starting' >停止</span>");
+		initializeTime();
+		starttime();
+		$('.current-time').css({visibility:"visible"});
+		$('#camera').addClass('btn-stop');
+		$('#camera').removeClass('btn-start');
+		//stopbox.append($('.current-time'));
+		stopLinkdiv.append(stopbox);
+		
+		/*stopLinkdiv.one("click",function(){
+			stopbox.html("<span class='starting'>停止中</span><i class='loading'></i>");
+			var url = "ajx/cameraBoxStop.do";
+			var data = "user=" + user + "&key=" + psk;
 			$.ajax({
 				type: "POST",
 				url: url,
@@ -86,28 +151,9 @@ function hideVideo(box, user, psk){
 						}, 5000);
 					}
 				}
-			});
-		});
-		box.append(startLinkdiv);
-	}
-}
-
-function showVideo(box, vid, user, psk){
-	if(box){
-		box.empty();
+			});*/
 		
-		var vlinkdiv = cje("div");
-		$('#view_video').html('');
-		//vlinkdiv.append(cje("a").attr("href", "play.do?video.videoId=" + vid).attr("target", "_blank").text("观看"));
-		$('#view_video').append(cje("a").attr("href", "play.do?video.videoId=" + vid).attr("target", "_blank").text("观看"));
-		//$('.view-area').attr("href", "play.do?video.videoId=" + vid).attr("target", "_blank"));
-		var stopLinkdiv = cje("div");
-		var stopbox = cje("div").html("<span class='starting'>停止</span>");
-		starttime();
-		$('.current-time').css({visibility:"visible"});
-		$('#camera').addClass('btn-stop');
-		$('#camera').removeClass('btn-start');
-		stopLinkdiv.append(stopbox);
+		
 		stopLinkdiv.click(function(){
 			stopbox.html("<span class='starting'>停止中</span><i class='loading'></i>");
 			var url = "ajx/cameraBoxStop.do";
@@ -120,6 +166,7 @@ function showVideo(box, vid, user, psk){
 				success: function(res){
 					if(res){
 						setTimeout(function(){
+							getBoxResponse(user);
 							getUserLiveVideo(user, function(vid){
 								showVideo(box, vid, user, psk);
 							}, function(){
@@ -173,84 +220,56 @@ function endBox(user,psk){
 		});
 }
 
-jQuery.cookie = function(name, value, options) {
-if (typeof value != 'undefined') {
-  options = options || {};
-  if (value === null) {
-  value = '';
-  options = $.extend({}, options);
-  options.expires = -1;
-  }
-  var expires = '';
-  if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
-  var date;
-  if (typeof options.expires == 'number') {
-   date = new Date();
-   date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
-  } else {
-   date = options.expires;
-  }
-  expires = '; expires=' + date.toUTCString();
-  }
-  var path = options.path ? '; path=' + (options.path) : '';
-  var domain = options.domain ? '; domain=' + (options.domain) : '';
-  var secure = options.secure ? '; secure' : '';
-  document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
-} else {
-  var cookieValue = null;
-  if (document.cookie && document.cookie != '') {
-  var cookies = document.cookie.split(';');
-  for (var i = 0; i < cookies.length; i++) {
-   var cookie = jQuery.trim(cookies[i]);
-   if (cookie.substring(0, name.length + 1) == (name + '=')) {
-   cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-   break;
-   }
-  }
-  }
-  return cookieValue;
-}
-};
-
-
-
 function settime(a){
 	if(a<10)
 		a = "0"+a;
 	return a;
 }
 
- if(($.cookie("cookieH")  == null) && ($.cookie("cookieM")  == null) && ($.cookie("cookieSe") == null) ){
- var h =0;  //timer--hour
- var m =0;  //timer--minute
- var s =0;  //timer--second
- }
- var t =0;  //setTimeout的返回标志
-
+function initializeTime(){
+	console.log("into ini_time");
+	console.log(typeof palyVideo);
+	if(palyVideo == null || palyVideo == "")
+	{
+		console.log("return");
+		return;}
+	else{
+   		console.log("videoID"+palyVideo);
+  		 $.ajax({
+			url:'ajx/m_calculateTime.do',
+			type:'post',
+			dataType:'json',
+			async:false,
+			data:"vid="+palyVideo,
+			success:function(res){
+				tt= res.result;
+				console.log(tt);
+				s = tt%60;//50
+  		    console.log("s="+s);
+  			var a = parseInt(tt/60);//131
+  			m = a%60;
+  			h = parseInt(a/60);
+				//tt =number res.result;
+				//console.log(tt);
+  			
+			}
+		});
+/*
+  		    s = tt%60;//50
+  		    console.log("s="+s);
+  			var a = parseInt(tt/60);//131
+  			m = a%60;
+  			h = parseInt(a/60);*/
+   		
+  	}
+}
+ 
 function starttime(){
-	$.cookie("cookieH",//写入cookie
-  h,//需要cookie写入的业务
-  {
-  "path":"/", //cookie的默认属性
-  "expires":2 //有效天数
-});
-$.cookie("cookieM",//写入cookie
-  m,//需要cookie写入的业务
-  {
-  "path":"/", //cookie的默认属性
-  "expires":2 //有效天数
-});
-$.cookie("cookieSe",//写入cookie
-  s,//需要cookie写入的业务
-  {
-  "path":"/", //cookie的默认属性
-  "expires":2 //有效天数
-});
-h = $.cookie("cookieH");
-m = $.cookie("cookieM");
-s = $.cookie("cookieSe");
-  
- // function begintime(){
+console.log("starttime");
+if(s==undefined){
+	$('.current-time').css({visibility:"hidden"});
+}
+	
   	var showh = settime(h);
 	var showm = settime(m);
 	var shows = settime(s);
@@ -270,18 +289,36 @@ s = $.cookie("cookieSe");
 	}
 
 	t = setTimeout("starttime()",1000);
-  //}
 		
 }
 
 
 function endtime() {
+	console.log("endtime");
 	clearTimeout(t);
-	h = 0;
-	m = 0;
-	s = 0;
-	$.cookie("cookieH", null);
-	$.cookie("cookieM", null);
-	$.cookie("cookieSe", null);
+	initializeTime();
+	//h = 0;
+	//m = 0;
+	//s = 0;
 }
-
+//获取
+function getBoxResponse(uid){
+	var url = "ajx/m_getBoxResponse.do";
+	var data = "uid="+uid;
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: data,
+		dataType: "JSON",
+		success: function(res){
+			if(res){
+				var result = res.result;
+				if(result!="启动视频成功" && result!="停止视频成功"){
+					alert("通信错误，请重试！");
+				}
+				console.log(result);
+			}
+		}
+	});
+	
+}
